246598714c837d1f991504ce286cabeecba1b6dd
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-07-19 20:28:09 +0530

    Update

--- project.py
+++ project.py
@@ -23,13 +23,15 @@ wiki = WikipediaQueryRun(wiki_wrapper = wiki_wrapper)
 search = DuckDuckGoSearchResults(name="Search")
 
 
-
+#streamlit title
 st.title("Langchain - Search")
 
 #Sidebar for Groq API Key
 st.sidebar.title("Settings")
 api_key = st.sidebar.text_input("Enter your Groq API Key:", type="password")
 
+
+# Initialize chat history if not present  
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {
@@ -39,5 +41,25 @@ if "messages" not in st.session_state:
 
     ]
 
+#display each message in chat bubble format
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    st.chat_message(msg["role"]).write(msg["content"]) 
+
+
+if prompt:=st.chat_input(placeholder="What is machine learning"):
+    
+    st.session_state.messages.append({
+        "role":"user",
+        "content":prompt
+    })
+
+    st.chat_message("user").write(prompt)  
+
+    llm = ChatGroq(groq_api_key = api_key,
+                   model_name = "Llama3-8b-8192",
+                   streaming=True)
+
+
+
+
+
