49912b821b26faeb584118f0222ddafb1350bac2
Soham Datta <153073640+HelloPenguin1@users.noreply.github.com>
2025-07-19 20:50:33 +0530

    Final update

--- project.py
+++ project.py
@@ -12,19 +12,19 @@ load_dotenv()
 #  Arxiv and Wiki tools
 arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, 
                                 doc_content_chars_max=200)
-arxiv = ArxivQueryRun(arxiv_wrapper = arxiv_wrapper)
+arxiv = ArxivQueryRun(api_wrapper = arxiv_wrapper)
 
 
 wiki_wrapper = WikipediaAPIWrapper(top_k_results=1,
                                    doc_content_chars_max=200)
-wiki = WikipediaQueryRun(wiki_wrapper = wiki_wrapper)
+wiki = WikipediaQueryRun(api_wrapper = wiki_wrapper)
 
 
 search = DuckDuckGoSearchResults(name="Search")
 
 
 #streamlit title
-st.title("Langchain - Search")
+st.title("Langchain - Search Engine with Chat")
 
 #Sidebar for Groq API Key
 st.sidebar.title("Settings")
@@ -46,8 +46,10 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"]) 
 
 
-if prompt:=st.chat_input(placeholder="What is machine learning"):
-    
+
+
+if prompt:=st.chat_input():
+
     st.session_state.messages.append({
         "role":"user",
         "content":prompt
@@ -58,6 +60,27 @@ if prompt:=st.chat_input(placeholder="What is machine learning"):
     llm = ChatGroq(groq_api_key = api_key,
                    model_name = "Llama3-8b-8192",
                    streaming=True)
+    
+    tools = [search, arxiv, wiki]
+
+    search_agent = initialize_agent(tools, 
+                                    llm, 
+                                    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors = True,
+                                    verbose = True) 
+
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(),
+                                         expand_new_thoughts=False)##shows thought of agent
+        
+        response  = search_agent.run(st.session_state.messages, callbacks = [st_cb])
+        st.session_state.messages.append({
+            'role':'assistant',
+            'content': response
+        })
+
+        st.write(response)
+
+
 
 
 
